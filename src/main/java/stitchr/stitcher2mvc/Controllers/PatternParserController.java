aa2ab32c958146756975b52eec2d0975fa4a9496
norrellborealis <tkkrings425@gmail.com>
2018-04-28 19:47:29 -0500

    crawler works but stuck in infinite loop and doesn't display to web app

--- src/main/java/stitchr/stitcher2mvc/Controllers/PatternParserController.java
+++ src/main/java/stitchr/stitcher2mvc/Controllers/PatternParserController.java
@@ -1,50 +1,23 @@
 package stitchr.stitcher2mvc.Controllers;
 
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import stitchr.stitcher2mvc.models.PatternCrawler;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.Callable;
-import org.jsoup.Jsoup;
 
 
 @Controller
-@RequestMapping("patterns")
 public class PatternParserController {
 
-    private HashSet<String> links;
+    @GetMapping("patterns")
+    public String displaypatterns(Model model) throws IOException {
+        model.addAttribute("title", "Patterns");
+        new PatternCrawler().getPatterns("http://www.allfreeknitting.com/tag/Full-Patterns", 0);
 
-    public PatternParserController() {
-        links = new HashSet<String>();
-    }
-
-    public void getPatterns(String URL) {
-        new PatternParserController().getPatterns("https://www.allfreeknitting.com/tag/Full-Patterns");
-
-        if (!links.contains(URL)) {
-            try {
-                if (links.add(URL)) {
-                    System.out.println(URL);
-                }
-
-                Document document = Jsoup.connect(URL).get();
-
-                Elements linksOnPage = document.select("a[href]");
-
-                for (Element page : linksOnPage) {
-                    getPatterns(page.attr("abs:href"));
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        return "redirect:/patterns";
     }
 
 }
---
+++ src/main/java/stitchr/stitcher2mvc/models/PatternCrawler.java
@@ -0,0 +1,41 @@
+package stitchr.stitcher2mvc.models;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+
+public class PatternCrawler {
+
+    private static final int MAX_DEPTH = 2;
+    private static HashSet<String> links;
+
+    public PatternCrawler() {
+
+        links = new HashSet<>();
+    }
+
+    public static void getPatterns(String URL, int depth) {
+        if ((!links.contains(URL) && (depth < MAX_DEPTH))) {
+            System.out.println(">> Depth: " + depth + " [" + URL + "]" );
+            try {
+                links.add(URL);
+
+                Document document = Jsoup.connect(URL).get();
+                Elements linksOnPage = document.select("a[href]");
+
+                depth++;
+                for (Element page : linksOnPage) {
+                    getPatterns(page.attr("abs:href"), depth);
+                }
+            } catch (IOException e) {
+                System.err.println("For '" + URL + "': " + e.getMessage());
+            }
+        }
+    }
+
+}
